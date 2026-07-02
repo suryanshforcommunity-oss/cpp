1c49820f7c1db3ef2ed14eb5d7aa4baec5f281ef
Suryansh10 <suryanshforcommunity@gmail.com>
2026-07-02 16:18:47 +0530

    Fix powerof2 syntax and add revArray.cpp

---
+++ revArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	int vector1[7] = {4, 7, 2, 8, 1, 3, 0};
+	int vector2[7];
+
+	 for (int i=0; i<=7; i++){
+	     
+	         vector2[7-i]=vector1[i];
+	    
+	 }
+
+	for(int i = 1; i <=7; i++)
+		cout << vector2[i] << ' ';
+	cout << endl;
+	return 0;
+}
