c82affd6ecb6f0a55b532600b4072ac680945f61
Suryansh10 <suryanshforcommunity@gmail.com>
2026-08-26 19:48:45 +0530

    Add patterns/addByfunction.cpp

---
+++ patterns/addByfunction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void sum(int num1 , int num2){
+    int num3=num1+num2;
+    cout<<num3;
+}
+int main(){
+    int num1, num2;
+    cin>>num1;
+    cin>>num2;
+    sum(num1,num2);
+    return 0;
+}
