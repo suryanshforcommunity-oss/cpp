4d4c8a55e06918f2ac4987d05a5947e956f3f0f9
Suryansh10 <suryanshforcommunity@gmail.com>
2026-08-26 19:48:49 +0530

    Add patterns/inc.cpp

---
+++ patterns/inc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter :";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=n;j>i;j--){
+            cout<<n-j+1;
+        }
+        cout<<endl;
+    }
+}
